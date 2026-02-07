772af144cb3041199b6930de45bb22203d25e553
rifqi <rifqialrasid71@gmail.com>
2026-02-07 23:10:30 +0700

    Update: Major connection fix

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+
+    // Tambahkan array ini (Sesuaikan dengan nama kolom di Database)
+    protected $fillable = [
+        'nama',
+        'no_hp',
+        'jenis_lensa',
+        'ukuran_kiri',
+        'ukuran_kanan',
+        'last_follow_up'
+    ];
 }
